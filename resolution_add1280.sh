8d351bcf68531b302a445df33cf1cf566192a1b0
horverno <horverno@gmail.com>
2021-09-15 08:33:35 +0200

    Add scripts from JKK PC

---
+++ resolution_add1280.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#sudo xrandr --newmode "1600x900_60.00"  118.25  1600 1696 1856 2112  900 903 908 934 -hsync +vsync
+sudo xrandr --newmode "1280x720_60.00"   74.50  1280 1344 1472 1664  720 723 728 748 -hsync +vsync
+#sudo xrandr --addmode HDMI-2 1600x900_60.00
+sudo xrandr --addmode HDMI-2 1280x720_60.00
+
---
+++ start_viz_remoteop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source /opt/ros/melodic/setup.bash
+source /home/jkk/autoware.ai/install/setup.bash
+source /home/jkk/Documents/swift_ros_ws/devel/setup.bash
+#source /home/jkk/catkin_ws/devel/setup.sh
+source /home/jkk/catkin_ws/devel/setup.bash
+
+export ROS_IP=192.168.1.2
+export ROS_MASTER_URI=http://192.168.1.5:11311
+
+export DISPLAY=:0
+screen -d -m -S view_zed bash -c 'rosrun rqt_image_view rqt_image_view /zed_node/left/image_rect_color/compressed'
+screen -d -m -S teleop_gui bash -c 'rosrun ros_guis teleop_gui2.py'
+screen -d -m -S teleop_rviz bash -c 'roslaunch nissan_bringup rviz03_teleop.launch'
+sleep 2
+wmctrl -r :ACTIVE: -e 5,0,0,1280,500
