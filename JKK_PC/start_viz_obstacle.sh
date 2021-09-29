6697588c0b00c32135ce41ec4f373d106200acb9
horverno <horverno@gmail.com>
2021-09-29 10:06:41 +0200

    Update on industrial PC

--- JKK_PC/start_viz_obstacle.sh
+++ JKK_PC/start_viz_obstacle.sh
@@ -4,17 +4,17 @@ export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:$LD_LIBRARY_PATH
 source /opt/ros/melodic/setup.bash
 source ~/leaf_ws/devel/setup.bash
 source ~/autoware.ai/install/local_setup.bash
-source ~/catkin_ws/devel/setup.bash
+source ~/catkin_ws/devel/setup.bash 
 #source ~/leaf_ws/devel/setup.bash
 # Set stuff
 export ROS_MASTER_URI=http://192.168.1.5:11311
 export ROS_IP=192.168.1.2
 
 export DISPLAY=:0
-sleep5
-screen -d -m -S view1 bash -c 'rosrun rviz rviz -d leaf04_elkerules_demo_v2.rviz'
 sleep 2
-screen -d -m -S view1 bash -c 'roslaunch nissan_bringup 3d_nissan.launch'
+screen -d -m -S view_nissan_rviz bash -c 'roslaunch nissan_bringup 3d_nissan.launch'
+sleep 5
+screen -d -m -S view_rviz_display bash -c 'rosrun rviz rviz -d leaf04_elkerules_demo_v2.rviz'
 sleep 1
 #wmctrl -r :ACTIVE: -e 5,0,0,1280,720
 #screen -d -m -S view2 bash -c 'rosrun rqt_image_view rqt_image_view /camera_stream/lidar_fusion/object_distances/compressed'
--- JKK_PC/start_viz_obstacle_drone.sh
+++ JKK_PC/start_viz_obstacle_drone.sh
@@ -2,9 +2,8 @@
 export PATH=/usr/local/cuda-10.0/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:$LD_LIBRARY_PATH
 source /opt/ros/melodic/setup.bash
-source ~/leaf_ws/devel/setup.bash
-source ~/autoware.ai/install/local_setup.bash
 source ~/catkin_ws/devel/setup.bash
+source ~/autoware.ai/install/local_setup.bash
 #source ~/leaf_ws/devel/setup.bash
 # Set stuff
 export ROS_MASTER_URI=http://192.168.1.5:11311
@@ -12,6 +11,10 @@ export ROS_IP=192.168.1.2
 
 export DISPLAY=:0
 sleep5
-screen -d -m -S view1 bash -c 'rosrun rviz rviz -d leaf04_elkerules_demo_drone.rviz'
+screen -d -m -S view_drone_rviz bash -c 'rosrun rviz_markers drone'
+sleep 2
+screen -d -m -S view_nissan_rviz bash -c 'roslaunch nissan_bringup 3d_nissan.launch'
+sleep 1
+screen -d -m -S view_rviz_display bash -c 'rosrun rviz rviz -d leaf04_elkerules_demo_drone.rviz'
 sleep 1
 wmctrl -r :ACTIVE: -e 5,0,0,1280,1024
